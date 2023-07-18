228b714bb4ad0971769158e9a147927332468bfa
TorchPochmak <exprompar@gmail.com>
2023-07-18 19:40:16 +0300

    cpp_boilerplate_changed

--- Boilerplate/_main.cpp
+++ Boilerplate/_main.cpp
@@ -1,62 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-typedef struct{
-    FILE* f;
-    char* name;
-    int size;
-} queue;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
 
+const ll INF = 1e18;
+const int INF_int = 1e9;
+const ll MOD = 1e9 + 7;
+const double PI = acos(-1.0);
 
-queue* Create(char* name){
-    queue* q = malloc(sizeof(queue));
-    q ->name = name;
-    q->size = 0;
+double __speedstart__;
+#define SPEEDTEST_START __speedstart__ = clock();
+#define SPEEDTEST_STOP cout << "Process finished in: " << (double)(clock() - __speedstart__)/CLOCKS_PER_SEC << " seconds";
+
+void solve()
+{
+    // let's go kid
 }
-
-void Push(queue* q, char el){
-    q->f = fopen(q->name, "a");
-    q->size++;
-    fprintf(q->f, "%c", el);
-    fclose(q->f);
-}
-char Pop(queue* q){
-    q->size--;
-    q->f = fopen(q->name, "r");
-    FILE* tmp = fopen("TMPTMPTMP.txt", "w");
-    char cur;
-    fscanf(q->f, "%c", &cur);
-    while(fscanf(q->f, "%c", &cur) != 0){
-        fprintf(tmp, "%c", cur);
-    }
-    fclose(tmp);
-    fclose(q->f);
-    tmp = fopen("TMPTMPTMP.txt", "r");
-    q->f = fopen(q->name, "w");
-    while(fscanf(tmp, "%c", &cur) != 0){
-        fprintf(q->f, "%c", cur);
-    }
-    fclose(tmp);
-    fclose(q->f);
-}
-
-int main(){
+int main()
+{
+    freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    queue q;
-    Create("input.txt");
-    Push(q, '3');
-    Push(q,'6');
-    
-    
-    // tree* tr;
-    // tr = create(5);
-    // add_node(tr, 5, 2);
-    // add_node(tr, 5, 3);
-    // add_node(tr, 3, 1);
-    // add_node(tr, 3, 10);
-    // //vecc.sort
-    // //comparator
-    // //swap
-    // printf("\n%d", dfscounter(tr->root));
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    SPEEDTEST_START
+    solve();
+    SPEEDTEST_STOP
 }
